db3b3e9f65bb061b9848cd2ee2847356acd39d96
FitStreet <fitstreety@gmail.com>
2024-02-05 20:03:13 +0600

    first commit

---
+++ account/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+
+    def _create_user(self, email, password, **kwargs):
+            if not email:
+                return ValueError('Почта должна обязательно передаватся')
+            email = self.normalize_email(email)
+            user = self.model(email=email, **kwargs)
+            user.create_activation_code()
+            user.set_password(password)
+            user.save()
+            return user
+    
+    def create_user(self, email, password, **kwargs):
+        kwargs.setdefault('is_staff', False)
+        kwargs.setdefault('is_superuser', False)
+        return self._create_user(email, password, **kwargs)
+
+    def create_superuser(self, email, password, **kwargs):
+        kwargs.setdefault('is_staff', True)
+        kwargs.setdefault('is_superuser', True)
+        kwargs.setdefault('is_active', True)
+
+        return self._create_user(email, password, **kwargs)
+    
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    activation_code = models.CharField(max_length=255, blank=True)
+    username = models.CharField(max_length=100, blank=True)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank = True)
+    is_active = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def create_activation_code(self):
+        import uuid
+        code = str(uuid.uuid4())
+        self.activation_code = code
+
+    def __str__(self) -> str:
+        return self.email
